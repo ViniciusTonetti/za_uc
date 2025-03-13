677110768a0f1dc37553715ca66d071b99337385
Vinicius Tonetti <vrtonetti@gmail.com>
2025-03-13 18:40:51 -0300

    atching za and uc

--- za_ucs.R
+++ za_ucs.R
@@ -1,2 +1,22 @@
 # Extracting values from ZAs around UCs in SP
 # Vinicius Tonetti - vrtonetti@gmail.com
+
+# Loading packages -------------------------------------------------------------
+
+library(terra)
+
+
+# Cleaning directory -----------------------------------------------------------
+
+rm(list = ls())
+
+
+# Exporting UCs from gpkg to shp -----------------------------------------------
+
+za <- vect("E:/_PESSOAL/Delano/za/vetores/za_ze_planos_uc_sp_conic.shp")
+uc <- vect("E:/_PESSOAL/Delano/za/vetores/SIGAM_uc_sp.gpkg")
+
+za[,"Nome"]
+unique(values(uc)[,"Unidade"])
+unique(values(za)[,"Nome"])
+
